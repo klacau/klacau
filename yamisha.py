5868b5657f325fadf4cff32a210110f5ff237523
Iosif Stalin <IosifDa@gmail.com>
2023-11-08 21:07:04 +0300

    добавил нужное

--- yamisha.py
+++ yamisha.py
@@ -1,3 +1,15 @@
-cout << "Difference between A and B: " << a - b << '\n';
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cout << "Enter A and B: ";
+    cin >> a >> b;
+    cout << "First number doubled: " << a * 2 << '\n';
+    cout << "Difference between A and B: " << a - b << '\n';
+}
+
+
 
 
